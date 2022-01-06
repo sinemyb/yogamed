f39960a27b152bfa55e515871b9cb832a60cc39f
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-01-06 12:17:29 +0300

    Add person.js

---
+++ person.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.photos = []
+  }
+
+  attend(event) {
+    this.event = event.name
+    event.attendees.push(this)
+  }
+
+  likeEvent(event) {
+    event.likedBy.push(this)
+  }
+
+  addPhoto(photo) {
+    this.photos.push(photo)
+  }
+}
+
+module.exports = Person
