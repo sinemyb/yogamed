82979981d7eb216f6073fbf8c5b6143bdf2b34b3
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-01-24 11:17:08 +0300

    Add docker-compose file

--- src/database-connection.js
+++ src/database-connection.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose')
 const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 const dbName = process.env.MONGODB_DATABASE
+let connectionString = process.env.MONGODB_CONNECTION_STRING
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
+}
 
 mongoose.set('debug', true)
 
 mongoose
-  .connect(`mongodb+srv://root:7wXDd5b9ZuwIFWGL@cluster0.hz1xl.mongodb.net/Cluster0?retryWrites=true&w=majority`, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
