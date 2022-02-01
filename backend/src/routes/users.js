de20553c7894d55cd17ba961d2a31307d41d78c5
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-02-01 12:10:08 +0300

    Update users.js for vuex

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,61 +1,61 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-const User = require('../models/user')
-const Photo = require('../models/photo')
-const Event = require('../models/event')
+const User = require("../models/user");
+const Photo = require("../models/photo");
+const Event = require("../models/event");
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
-  const query = {}
+router.get("/", async (req, res) => {
+  const query = {};
 
   if (req.query.name) {
-    query.name = req.query.name
+    query.name = req.query.name;
   }
 
   if (req.query.age) {
-    query.age = req.query.age
+    query.age = req.query.age;
   }
 
-  res.send(await User.find(query))
-})
+  res.send(await User.find(query));
+});
 
 /* POST create a user */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const userToCreate = {
     name: req.body.name,
     age: req.body.age,
-  }
+  };
 
-  const createdUser = await User.create(userToCreate)
-  res.send(createdUser)
-})
+  const createdUser = await User.create(userToCreate);
+  res.send(createdUser);
+});
 
-router.get('/initialize', async (req, res) => {
+router.get("/initialize", async (req, res) => {
   // const users = []
   // printName = person => console.log(person.name)
-  const ceyhan = await User.create({ name: 'ceyhan', age: 32 })
-  const sinem = await User.create({ name: 'sinem', age: 36 })
+  const ceyhan = await User.create({ name: "ceyhan", age: 32 });
+  const sinem = await User.create({ name: "sinem", age: 36 });
 
-  const centralparkPhoto = await Photo.create({ filename: 'centralpark.jpg' })
-  const ycp = await Event.create('Yoga class in Central Park')
+  const centralparkPhoto = await Photo.create({ filename: "centralpark.jpg" });
+  const ycp = await Event.create("Yoga class in Central Park");
   // event is still not object, it is string. This needs to be changed.
 
   // centralparkPhoto.save()
 
   // const ycp = new Event('Yoga class in Central Park')
 
-  await sinem.likeEvent(ycp)
-  await ceyhan.likeEvent(ycp)
-  await sinem.addPhoto(centralparkPhoto)
-  await ceyhan.addPhoto(centralparkPhoto)
-  await sinem.attend(ycp)
-  await ceyhan.attend(ycp)
+  await sinem.likeEvent(ycp);
+  await ceyhan.likeEvent(ycp);
+  await sinem.addPhoto(centralparkPhoto);
+  await ceyhan.addPhoto(centralparkPhoto);
+  await sinem.attend(ycp);
+  await ceyhan.attend(ycp);
 
-  console.log(sinem)
-  res.sendStatus(200)
-})
+  console.log(sinem);
+  res.sendStatus(200);
+});
 
 // ycp.printAttendeeNames()
 
@@ -65,40 +65,40 @@ router.get('/initialize', async (req, res) => {
 
 //  const users = [ceyhan, sinem]
 
-router.post('/:userId/adds', async (req, res) => {
-  const user = await User.findById(req.params.userId)
-  const photo = await Photo.findById(req.body.photoId)
+router.post("/:userId/adds", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  const photo = await Photo.findById(req.body.photoId);
 
-  await user.addPhoto(photo)
-  res.sendStatus(200)
-})
+  await user.addPhoto(photo);
+  res.sendStatus(200);
+});
 
-router.post('/:userId/likes', async (req, res) => {
-  const user = await User.findById(req.params.userId)
-  const event = await Event.findById(req.body.eventId)
+router.post("/:userId/likes", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  const event = await Event.findById(req.body.eventId);
 
-  await user.likeEvent(event)
-  res.sendStatus(200)
-})
+  await user.likeEvent(event);
+  res.sendStatus(200);
+});
 
-router.post('/:userId/attends', async (req, res) => {
-  const user = await User.findById(req.params.userId)
-  const event = await Event.findById(req.body.eventId)
+router.post("/:userId/attends", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  const event = await Event.findById(req.body.eventId);
 
-  await user.attend(event)
-  res.sendStatus(200)
-})
+  await user.attend(event);
+  res.sendStatus(200);
+});
 
-router.get('/:userId', async (req, res) => {
-  const user = await User.findById(req.params.userId)
+router.get("/:userId", async (req, res) => {
+  const user = await User.findById(req.params.userId);
 
-  if (user) res.render('user', { user })
-  else res.sendStatus(404)
-})
+  if (user) res.send(user);
+  else res.sendStatus(404);
+});
 
-router.get('/:userId/json', async (req, res) => {
-  const user = await User.findById(req.params.userId)
-  res.send(user)
-})
+router.get("/:userId/json", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  res.send(user);
+});
 
-module.exports = router
+module.exports = router;
