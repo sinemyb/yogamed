9d5e57d1085a26099c2d662493bce5803576f0e2
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-03-13 16:47:48 +0300

    Add routes

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,23 +1,25 @@
 const express = require("express");
-
 const router = express.Router();
 const Event = require("../models/event");
 
-/* POST create an event */
-router.post("/", async (req, res) => {
-  const eventToCreate = {
-    name: req.body.name,
-  };
+router.get("/:eventId", async (req, res) => {
+  const event = await Event.findById(req.params.eventId);
 
-  const createdEvent = await Event.create(eventToCreate);
-  res.send(createdEvent);
+  if (event) res.send(event);
+  else res.sendStatus(404);
 });
 
-// router.post("/:eventId/attendances", async (req, res) => {
-//   const event = await Event.findById(req.params.eventId);
+router.post("/:eventId/adds", async (req, res) => {
+  const event = await Event.findById(req.params.eventId);
 
-//   await event.attend(req.user);
-//   res.sendStatus(200);
-// });
+  await event.addEvent(event);
+  res.sendStatus(200);
+});
+
+router.get("/:eventId/json", async (req, res) => {
+  const event = await Event.findById(req.params.eventId);
+  res.send(event);
+});
 
 module.exports = router;
+
